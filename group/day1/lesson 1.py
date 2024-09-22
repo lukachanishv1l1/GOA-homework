b5183651f25b7a10a7bf7633975b3a21d4b3259c
lukachanishv1l1 <chanishvililuka0@gmail.com>
2024-09-22 11:24:56 +0400

    first commit

---
+++ group/day1/lesson 1.py
@@ -0,0 +1,26 @@
+name ="luka"
+
+
+surname ="chanishvili"
+
+print(name)
+
+age=14
+height= 160.4
+
+know_programming = True 
+is_ugly = False
+
+isugly = False
+
+print(name + " " +surname)
+
+
+print(type(age)) 
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(know_programming))
+
+print(name + "  " +str(age))
+print(name + "  " + str(surname)+"  "+str(age)+"  "+str(height))
